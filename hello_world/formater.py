32db5a46b7c450410367cfddf8d0f933f9e2d528
blackieghoster <blackieghoster@gmail.com>
2022-10-16 17:18:42 +0200

    Code refractor

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -1,4 +1,3 @@
-
 PLAIN = "plain"
 PLAIN_UP = "plain_uppercase"
 PLAIN_LO = "plain_lowercase"
@@ -7,31 +6,32 @@ JSON = "json"
 SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON]
 
 
-def get_formatted(msg, imie, format):
+def get_formatted(msg, name, format):
     result = ""
+
     if format == PLAIN:
-        result = plain_text(msg, imie)
+        result = plain_text(msg, name)
     elif format == PLAIN_UP:
-        result = plain_text_upper_case(msg, imie)
+        result = plain_text_upper_case(msg, name)
     elif format == PLAIN_LO:
-        result = plain_text_lower_case(msg, imie)
+        result = plain_text_lower_case(msg, name)
     elif format == JSON:
-        result = format_to_json(msg, imie)
+        result = format_to_json(msg, name)
     return result
 
 
-def format_to_json(msg, imie):
-    return ('{ "imie":"' + imie + '", "mgs":' +
+def format_to_json(msg, name):
+    return ('{ "Name":"' + name + '", "mgs":' +
             msg + '"}')
 
 
-def plain_text(msg, imie):
-    return imie + ' ' + msg
+def plain_text(msg, name):
+    return name + ' ' + msg
 
 
-def plain_text_upper_case(msg, imie):
-    return plain_text(msg.upper(), imie.upper())
+def plain_text_upper_case(msg, name):
+    return plain_text(msg.upper(), name.upper())
 
 
-def plain_text_lower_case(msg, imie):
-    return plain_text(msg.lower(), imie.lower())
+def plain_text_lower_case(msg, name):
+    return plain_text(msg.lower(), name.lower())
