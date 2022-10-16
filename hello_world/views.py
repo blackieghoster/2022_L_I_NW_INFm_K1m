56a2d072500c30c07f09fd8c5ffcadb48f0a14e5
blackieghoster <blackieghoster@gmail.com>
2022-10-16 17:16:26 +0200

    Code refractor

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,16 +3,18 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Natalia"
+user_name = "John Doe"
 msg = "Hello World!"
+
 
 @app.route('/')
 def index():
     output = request.args.get('output')
     if not output:
         output = PLAIN
-    return get_formatted(msg, moje_imie,
+    return get_formatted(msg, user_name,
                          output.lower())
+
 
 @app.route('/outputs')
 def supported_output():
